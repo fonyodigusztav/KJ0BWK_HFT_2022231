30385ea54186964749bac756613b76a42cd2bad7
fonyodigusztav <fonyodi.guszto@gmail.com>
2022-10-25 13:59:58 +0200

    previous reference settings deleted, it is now really set in logic

--- KJ0BWK_HFT_2022231.Client/Program.cs
+++ KJ0BWK_HFT_2022231.Client/Program.cs
@@ -2,8 +2,6 @@
 using KJ0BWK_HFT_2022231.Repository;
 using System;
 using System.Linq;
-using KJ0BWK_HFT_2022231.Models;
-using KJ0BWK_HFT_2022231.Repository;
 
 namespace KJ0BWK_HFT_2022231.Client
 {
