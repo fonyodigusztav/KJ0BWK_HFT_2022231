f6071332acedd3aa7ab2ca18bcd00033ad214fd6
fonyodigusztav <fonyodi.guszto@gmail.com>
2022-10-25 11:32:30 +0200

    DbContext.cs is created in Repository project, basic properties and ctor added

---
+++ KJ0BWK_HFT_2022231.Repository/FootballDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using KJ0BWK_HFT_2022231.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KJ0BWK_HFT_2022231.Repository
+{
+    public class FootballDbContext: DbContext
+    {
+        public DbSet<Player> Players { get; set; }
+        public DbSet<Club> Clubs { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+        public FootballDbContext()
+        {
+            this.Database.EnsureCreated();
+        }
+    }
+}
