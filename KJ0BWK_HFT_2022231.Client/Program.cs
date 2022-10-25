bab062476311d506f34d41f5d187f4ea06fad6ce
fonyodigusztav <fonyodi.guszto@gmail.com>
2022-10-25 15:22:02 +0200

    Client Project is set with a console menu application, it is not finished yet

--- KJ0BWK_HFT_2022231.Client/Program.cs
+++ KJ0BWK_HFT_2022231.Client/Program.cs
@@ -1,4 +1,5 @@
-﻿using KJ0BWK_HFT_2022231.Logic;
+﻿using ConsoleTools;
+using KJ0BWK_HFT_2022231.Logic;
 using KJ0BWK_HFT_2022231.Models;
 using KJ0BWK_HFT_2022231.Repository;
 using System;
@@ -8,6 +9,37 @@ namespace KJ0BWK_HFT_2022231.Client
 {
     class Program
     {
+        static PlayerLogic playerLogic;
+        static ClubLogic clubLogic;
+        static OwnerLogic ownerLogic;
+        static void Create(string entity)
+        {
+            Console.WriteLine(entity + " create");
+            Console.ReadLine();
+        }
+        static void List(string entity)
+        {
+            if (entity == "Player")
+            {
+                var items = playerLogic.ReadAll();
+                Console.WriteLine("ID" + "\t" + "Name");
+                foreach (var item in items)
+                {
+                    Console.WriteLine(item.PlayerID + "\t" + item.Name);
+                }
+                Console.ReadLine();
+            }
+        }
+        static void Update(string entity)
+        {
+            Console.WriteLine(entity + " update");
+            Console.ReadLine();
+        }
+        static void Delete(string entity)
+        {
+            Console.WriteLine(entity + " delete");
+            Console.ReadLine();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
@@ -19,9 +51,39 @@ namespace KJ0BWK_HFT_2022231.Client
             var clubRepo = new ClubRepository(ctx);
             var ownerRepo = new OwnerRepository(ctx);
 
-            var playerLogic = new PlayerLogic(playerRepo);
-            var clubLogic = new ClubLogic(clubRepo);
-            var ownerLogic = new OwnerLogic(ownerRepo);
+            playerLogic = new PlayerLogic(playerRepo);
+            clubLogic = new ClubLogic(clubRepo);
+            ownerLogic = new OwnerLogic(ownerRepo);
+
+
+            var ownerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Owner"))
+                .Add("Create", () => Create("Owner"))
+                .Add("Delete", () => Delete("Owner"))
+                .Add("Update", () => Update("Owner"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var clubSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Club"))
+                .Add("Create", () => Create("Club"))
+                .Add("Delete", () => Delete("Club"))
+                .Add("Update", () => Update("Club"))
+                .Add("Exit", ConsoleMenu.Close);
+
+            var playerSubMenu = new ConsoleMenu(args, level: 1)
+                .Add("List", () => List("Player"))
+                .Add("Create", () => Create("Player"))
+                .Add("Delete", () => Delete("Player"))
+                .Add("Update", () => Update("Player"))
+                .Add("Exit", ConsoleMenu.Close);
+            
+            var menu = new ConsoleMenu(args, level: 0)
+                .Add("Players", () => playerSubMenu.Show())
+                .Add("Clubs", () => clubSubMenu.Show())
+                .Add("Owners", () => ownerSubMenu.Show())
+                .Add("Exit", ConsoleMenu.Close);
+            
+            menu.Show();
             ;
         }
     }
