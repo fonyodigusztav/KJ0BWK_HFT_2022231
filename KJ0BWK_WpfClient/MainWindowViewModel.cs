d5c3b0f4f257ecd091a3d435fb3c6364de09b07e
gusztav <fonyodi.guszto@gmail.com>
2023-05-18 11:07:00 +0200

    wpf created

---
+++ KJ0BWK_WpfClient/MainWindowViewModel.cs
@@ -0,0 +1,18 @@
+﻿using KJ0BWK_HFT_2022231.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KJ0BWK_WpfClient
+{
+    public class MainWindowViewModel
+    {
+        public RestCollection<Player> Players { get; set; }
+        public MainWindowViewModel()
+        {
+            Players = new RestCollection<Player>("http://localhost:64355/", "player");
+        }
+    }
+}
