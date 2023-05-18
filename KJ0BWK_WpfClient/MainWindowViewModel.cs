f16302f1229f6c3a36c87e1dbe640a388e07dc2d
gusztav <fonyodi.guszto@gmail.com>
2023-05-18 11:52:56 +0200

    commands

--- KJ0BWK_WpfClient/MainWindowViewModel.cs
+++ KJ0BWK_WpfClient/MainWindowViewModel.cs
@@ -1,18 +1,53 @@
 ﻿using KJ0BWK_HFT_2022231.Models;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using Microsoft.Toolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace KJ0BWK_WpfClient
 {
-    public class MainWindowViewModel
+    public class MainWindowViewModel :ObservableRecipient
     {
         public RestCollection<Player> Players { get; set; }
+        private Player selectedPlayer;
+
+        public Player SelectedPlayer
+        {
+            get { return selectedPlayer; }
+            set 
+            { 
+                SetProperty(ref selectedPlayer, value);
+                (DeletePlayerCommand as RelayCommand).NotifyCanExecuteChanged();
+            }
+        }
+
+        public ICommand CreatePlayerCommand { get; set; }
+        public ICommand DeletePlayerCommand { get; set; }
+        public ICommand UpdatePlayerCommand { get; set; }
         public MainWindowViewModel()
         {
             Players = new RestCollection<Player>("http://localhost:64355/", "player");
+            CreatePlayerCommand = new RelayCommand(() =>
+            {
+                Players.Add(new Player()
+                {
+                    Name = "DavidLuiz",
+                    Age = 18
+                });
+            });
+
+            DeletePlayerCommand = new RelayCommand(() =>
+            {
+                Players.Delete(SelectedPlayer.PlayerID);
+            },
+            () =>
+            {
+                return SelectedPlayer != null;
+            });
         }
     }
 }
